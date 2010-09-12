a7cde79f0377323bbcc3b05d775e41371751c8a1
Nicholas Peter Bamber <nicholas@leonhartsberger.periapt>
2010-09-12 13:58:58 +0100

    changed order

--- lib/Debian/Debhelper/Sequence/components.pm
+++ lib/Debian/Debhelper/Sequence/components.pm
@@ -3,7 +3,7 @@ use warnings;
 use strict;
 use Debian::Debhelper::Dh_Lib;
 
-insert_before("dh_installchangelogs", "dh_components");
+insert_before("dh_installdirs", "dh_components");
 #insert_before("dh_clean", "dh_components_purge");
 
 1;
